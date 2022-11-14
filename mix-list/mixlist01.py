60448a9ce121ca56582bb60928835cbd78c2fa14
ronaldProctor <ron@proctorcreative.com>
2022-11-14 19:04:36 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,11 @@
+#!/bin/usr/env python3
+
+"""Define the list"""
+my_list = ["192.168.0.5", 5060, "UP"]
+
+"""Print the list items"""
+print("The first item in the list (IP): " + my_list[0])
+print("The second item in the list(port): " + str(my_list[1]))
+print("The last item in the list (state): " + my_list[2])
+
+
