1a81e4f5ab1d16dade0252afbd12dcf206c044ba
ronaldProctor <ron@proctorcreative.com>
2022-11-30 16:36:24 +0000

    commit.py

---
+++ commit.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env
+"""Proctor Creative LLC | ron@proctorcreative.com
+    Automagic git commit and push."""
+
+# get smarter
+import os
+
+print("\nCommitting to repository ...\n")
+
+comment = input("Please enter your commit comment: ")  # get user comment for commit
+
+if comment == "":
+    comment = "No comment."
+
+print("Command: cd ~/mycode")
+os.system("cd ~/mycode")
+
+print("Command: pwd")
+os.system("pwd")
+
+print("Command: git add *")
+os.system("git add *")
+
+commandstring = "git commit -m " + comment
+print(commandstring)
+os.system(commandstring)
+
+print("Command: git push origin HEAD")
+os.system("git push origin HEAD")
+
+
